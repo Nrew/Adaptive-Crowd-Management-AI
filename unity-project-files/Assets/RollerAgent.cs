b71c72fac7aca37cb7a835b8b8578929f1f57b4b
Dan Tkachov <tkachov.d@northeastern.edu>
2024-11-21 01:17:45 -0500

    Updated training script in trainingscript.py

--- unity-project-files/Assets/RollerAgent.cs
+++ unity-project-files/Assets/RollerAgent.cs
@@ -1,69 +1,163 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Unity.MLAgents;
-using Unity.MLAgents.Sensors;
-using Unity.MLAgents.Actuators;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Unity.MLAgents;
+//using Unity.MLAgents.Sensors;
+//using Unity.MLAgents.Actuators;
 
-public class RollerAgent : Agent
-{
-    // Start is called before the first frame update
+//public class RollerAgent : Agent
+//{
+//    // Start is called before the first frame update
 
-    Rigidbody rBody;
-    void Start()
-    {
-        rBody = GetComponent<Rigidbody>();
-    }
-    public Transform Target;
-    public override void OnEpisodeBegin()
-    {
-        if (this.transform.localPosition.y < 0)
-        {
-            this.rBody.angularVelocity = Vector3.zero;
-            this.rBody.velocity = Vector3.zero;
-            this.transform.localPosition = new Vector3 (0, 0.5f, 0);
-        }
-        Target.localPosition  = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
-    }
+//    Rigidbody rBody;
 
-    public override void CollectObservations(VectorSensor sensor)
-    {
-        sensor.AddObservation(Target.localPosition);
-        sensor.AddObservation(this.transform.localPosition);
+//    //public Transform Target;
+//    public float forceMultiplier = 10;
 
-        sensor.AddObservation(rBody.velocity.x);
-        sensor.AddObservation(rBody.velocity.z);
-    }
 
-    public float forceMultiplier = 10;
-    public override void OnActionReceived(ActionBuffers actionBuffers)
-    {
-        Vector3 controlSignal = Vector3.zero;
-        controlSignal.x = actionBuffers.ContinuousActions[0];
-        controlSignal.z = actionBuffers.ContinuousActions[1];
-        rBody.AddForce(controlSignal * forceMultiplier);
+//    public Vector3 areacenter = new Vector3(-40f, 0f, 12f);
+//    public Vector2 areaSize = new Vector2(10f, 10f);
+//    private static bool targetAreaCreated = false;
+//    public GameObject targetAreaVisual;
 
-        // Rewards
-        float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+//    private static HashSet<RollerAgent> agentsInTargetArea = new HashSet<RollerAgent>();
+//    private bool hasReachedTargetArea = false;
+//    private static int totalAgents = 3;
 
-        // Reached Target
-        if(distanceToTarget < 1.42f)
-        {
-            SetReward(1.0f);
-            EndEpisode();
-        }
-        else if (this.transform.localPosition.y < 0)
-        {
-            EndEpisode();
-        }
 
-    }
+//    void Start()
+//    {
+//        rBody = GetComponent<Rigidbody>();
+//        if (!targetAreaCreated)
+//        {
+//            CreateTargetArea();
+//            targetAreaCreated = true;
+//        }
+        
+//    }
+//    public override void OnEpisodeBegin()
+//    {
+//        if (this.transform.localPosition.y < 0)
+//        {
+//            this.rBody.angularVelocity = Vector3.zero;
+//            this.rBody.linearVelocity = Vector3.zero;
+//            this.transform.localPosition = new Vector3 (0, 0.5f, 0);
+//        }
 
-    public override void Heuristic(in ActionBuffers actionsOut)
-    {
-        var continuousActionsOut = actionsOut.ContinuousActions;
-        continuousActionsOut[0] = Input.GetAxis("Horizontal");
-        continuousActionsOut[1] = Input.GetAxis("Vertical");
-    }
+//    }
 
-}
+//    void CreateTargetArea()
+//    {
+//        // Set the position and scale of the target area
+//        targetAreaVisual = GameObject.CreatePrimitive(PrimitiveType.Cube);
+//        targetAreaVisual.transform.localScale = new Vector3(areaSize.x, .1f, areaSize.y);
+//        targetAreaVisual.transform.position = new Vector3(areacenter.x, areacenter.y, areacenter.z);
+
+//        // Make it green and transparent to easily identify it
+//        Renderer renderer = targetAreaVisual.GetComponent<Renderer>();
+//        Material material = new Material(Shader.Find("standard"));
+//        material.color = new Color(0f, 1f, 0f, .5f);
+//        renderer.material = material;
+
+//        // Make it non-physical so that there are no collisions
+//        Destroy(targetAreaVisual.GetComponent<Collider>());
+//    }
+
+//    public override void CollectObservations(VectorSensor sensor)
+//    {
+//        sensor.AddObservation(areacenter);
+//        sensor.AddObservation(this.transform.localPosition);
+
+//        sensor.AddObservation(rBody.linearVelocity.x);
+//        sensor.AddObservation(rBody.linearVelocity.z);
+//        sensor.AddObservation(hasReachedTargetArea);
+//    }
+
+//    private bool isInTargetArea()
+//    {
+//        Vector3 agentPos = this.transform.position;
+//        float halfWidth = areaSize.x / 2;
+//        float halfLength = areaSize.y / 2;
+
+//        return agentPos.x >= (areacenter.x - halfWidth)
+//            && agentPos.y >= (areacenter.z - halfLength)
+//            && agentPos.x <= (areacenter.x + halfWidth) 
+//            && agentPos.y <= (areacenter.z + halfLength);
+//    }
+//    private bool AllAgentsInTargetArea()
+//    {
+//        return agentsInTargetArea.Count >= totalAgents;
+//    }
+
+//    public override void OnActionReceived(ActionBuffers actionBuffers)
+//    {
+//        if (!hasReachedTargetArea)
+//        {
+//            Vector3 controlSignal = Vector3.zero;
+//            controlSignal.x = actionBuffers.ContinuousActions[0];
+//            controlSignal.z = actionBuffers.ContinuousActions[1];
+//            rBody.AddForce(controlSignal * forceMultiplier);
+//        }
+
+//        if (isInTargetArea() && !hasReachedTargetArea)
+//        {
+//            hasReachedTargetArea = true;
+//            agentsInTargetArea.Add(this);
+//            SetReward(10.0f);
+
+//            if (AllAgentsInTargetArea())
+//            {
+//                RollerAgent[] allAgents = Object.FindObjectsByType<RollerAgent>(FindObjectsSortMode.None);
+//                foreach (RollerAgent agent in allAgents)
+//                {
+//                    agent.EndEpisode();
+//                }
+//            }
+//        }
+//        // If it falls off the edge, end all episodes and reset
+//        else if (this.transform.localPosition.y < -1)
+//        {
+//            SetReward(-5f);
+//            RollerAgent[] allAgents = Object.FindObjectsByType<RollerAgent>(FindObjectsSortMode.None);
+//            foreach (RollerAgent agent in allAgents)
+//            {
+//                agent.EndEpisode();
+//            }
+//        }
+//        else if (!hasReachedTargetArea)
+//        {
+//            float distanceToAreaCenter = Vector3.Distance(this.transform.localPosition, areacenter);
+//            AddReward(-0.001f * distanceToAreaCenter);
+//        }
+
+
+//    }
+
+//    public override void Heuristic(in ActionBuffers actionsOut)
+//    {
+//        var continuousActionsOut = actionsOut.ContinuousActions;
+//        continuousActionsOut[0] = Input.GetAxis("Horizontal");
+//        continuousActionsOut[1] = Input.GetAxis("Vertical");
+//    }
+
+//    void OnDestroy()
+//    {
+//        // Decrement total agent count when an agent is destroyed
+//        totalAgents--;
+//        agentsInTargetArea.Remove(this);
+
+//        // Clean up target area if this is the last agent
+//        if (targetAreaVisual != null && gameObject.scene.isLoaded)
+//        {
+//            RollerAgent[] remainingAgents = Object.FindObjectsByType<RollerAgent>(FindObjectsSortMode.None);
+//            if (remainingAgents.Length <= 1)
+//            {
+//                Destroy(targetAreaVisual);
+//                targetAreaCreated = false;
+//                agentsInTargetArea.Clear();
+//                totalAgents = 0;
+//            }
+//        }
+//    }
+
+//}
